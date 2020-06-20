14bf32486533788ba5f836210cd006251714bcc1
kirubarajm <kirubarajmathavan@gmail.com>
2020-06-20 21:23:42 +0530

    Added Filters in procurment list

--- model/procurement/procurementModel.js
+++ model/procurement/procurementModel.js
@@ -51,7 +51,14 @@ Procurement.new_procurement_create=async function new_procurement_create(new_Pro
 /////// Get Precurment List ///////////
 Procurement.procurement_list=async function procurement_list(req,result) {
   if(req.zone_id){
-    var procurement_list_query= "select pro.*,pm.Productname,pm.uom as unit,um.name as unit_name,st.quantity as boh,greatest(0,pro.quantity-st.quantity)as procurement_quantity from Procurement  pro left join Product_live pl on pl.vpid=pro.vpid left join ProductMaster pm on pm.pid=pl.pid left join UOM um on um.uomid=pm.uom left join Stock st on st.vpid= pm.pid where pro.pr_status=1 and pro.zoneid='"+req.zone_id+"'";    
+    var where = "";    
+    if(req.date){
+        where = where+" and date(pro.created_at)='"+req.date+"' ";
+    }
+    if(req.vpid){
+        where = where+" and pro.vpid="+req.vpid;
+    }
+    var procurement_list_query= "select pro.*,pm.Productname,pm.uom as unit,um.name as unit_name,st.quantity as boh,greatest(0,pro.quantity-st.quantity)as procurement_quantity from Procurement  pro left join Product_live pl on pl.vpid=pro.vpid left join ProductMaster pm on pm.pid=pl.pid left join UOM um on um.uomid=pm.uom left join Stock st on st.vpid= pm.pid where pro.pr_status=1 and pro.zoneid="+req.zone_id+" "+where+"";    
     var procurement_list = await query(procurement_list_query);
 
     if(procurement_list.length > 0){
