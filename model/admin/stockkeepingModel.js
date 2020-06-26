6a2b783923aef448a22e2ce56b3c4571a8e81628
kirubarajm <kirubarajmathavan@gmail.com>
2020-06-26 18:12:24 +0530

    updated in stock for stock keeping

--- model/admin/stockkeepingModel.js
+++ model/admin/stockkeepingModel.js
@@ -22,7 +22,7 @@ StockKeeping.stockkeeping_list =async function stockkeeping_list(req,result) {
     if(req.zone_id){
         var where = "";
         if(req.cat_id){
-            where = where+" and cat_id="+re.cat_id+" ";
+            where = where+" and cat_id="+req.cat_id+" ";
         }
         if(req.scl1_id){
             where = where+" and scl1_id="+req.scl1_id+" ";
@@ -94,44 +94,64 @@ StockKeeping.stockkeeping_openlist =async function stockkeeping_openlist(req,res
 /////////Stockkeeping Add///////////
 StockKeeping.stockkeeping_add =async function stockkeeping_add(req,result) {
     if(req.zone_id && req.stockid && req.vpid){
-        //var checkSKquery = "select"
-        if(req.actual_quantity){  var actual_quantity=req.actual_quantity; }else{ var actual_quantity=0; }
-        if(req.missing_quantity){  var missing_quantity=req.missing_quantity; }else{ var missing_quantity=0; }
-        if(req.wastage){  var wastage=req.wastage; }else{ var wastage=0; }
-        if(req.wastage_image){  var wastage_image=req.wastage_image; }else{ var wastage_image=0; }
-        if(req.type){  var type=req.type; }else{ var type=0; }        
+        var checkSKquery = "select * from StockKeeping where zoneid="+req.zone_id+" and stockid="+req.stockid+" and vpid="+req.vpid+" and date(created_at)=CURDATE()";
+        var checkS = await query(checkSKquery);
+        if(checkS.length==0){
+            if(req.actual_quantity){  var actual_quantity=req.actual_quantity; }else{ var actual_quantity=0; }
+            if(req.missing_quantity){  var missing_quantity=req.missing_quantity; }else{ var missing_quantity=0; }
+            if(req.wastage){  var wastage=req.wastage; }else{ var wastage=0; }
+            if(req.wastage_image){  var wastage_image=req.wastage_image; }else{ var wastage_image=""; }
+            if(req.type){  var type=req.type; }else{ var type=0; }        
 
-        var getotherquery = "select st.stockid,st.vpid,pm.Productname,pm.weight,cat.catid,cat.name as catagory_name,scl1.scl1_id,scl1.name as subcatL1name,scl2.scl2_id,scl2.name as subcatL2name,pm.uom as uomid,uom.name as uom_name,st.quantity as boh,pm.mrp,sum(dop.received_quantity) as insorting from Stock as st left join Product_live as pl on pl.vpid=st.vpid left join ProductMaster as pm on pm.pid=pl.pid left join SubcategoryL2 as scl2 on scl2.scl2_id=pm.scl2_id left join SubcategoryL1 as scl1 on scl1.scl1_id=pm.scl1_id left join Category as cat on cat.catid=scl1.catid left join UOM as uom on uom.uomid=pm.uom left join Dayorder_products as dop on dop.vpid=st.vpid where st.vpid="+req.stockid+" and st.zoneid="+req.zone_id+" and st.vpid="+req.vpid+" and dop.scm_status=3 group by st.vpid";
-        var getother = await query(getotherquery);
+            var getotherquery = "select st.stockid,st.vpid,pm.Productname,pm.weight,cat.catid,cat.name as catagory_name,scl1.scl1_id,scl1.name as subcatL1name,scl2.scl2_id,scl2.name as subcatL2name,pm.uom as uomid,uom.name as uom_name,st.quantity as boh,pm.mrp,sum(dop.received_quantity) as insorting from Stock as st left join Product_live as pl on pl.vpid=st.vpid left join ProductMaster as pm on pm.pid=pl.pid left join SubcategoryL2 as scl2 on scl2.scl2_id=pm.scl2_id left join SubcategoryL1 as scl1 on scl1.scl1_id=pm.scl1_id left join Category as cat on cat.catid=scl1.catid left join UOM as uom on uom.uomid=pm.uom left join Dayorder_products as dop on dop.vpid=st.vpid where st.vpid="+req.stockid+" and st.zoneid="+req.zone_id+" and st.vpid="+req.vpid+" and dop.scm_status=3 group by st.vpid";
+            var getother = await query(getotherquery);
 
-        if(getother.length>0){
-            var sklist = [];
-            sklist.push({"stockid":req.stockid,"vpid":req.vpid,"product_name":getother[0].Productname,"cat_id":getother[0].catid,"category_name":getother[0].catagory_name,"scl1_id":getother[0].scl1_id,"subcategoryl1_name":getother[0].subcatL1name,"scl2_id":getother[0].scl2_id,"subcategoryl2_name":getother[0].subcatL2name,"price":getother[0].mrp,"missing_quantity":missing_quantity,"boh":getother[0].boh,"actual_quantity":actual_quantity,"in_sorting":getother[0].insorting,"type":type,"wastage":wastage,"wastage_image":wastage_image,"zone_id":req.zone_id,"commend":getother[0].Productname,"weight":getother[0].weight});
-        
-            Stockkeeping.createStockKeeping(sklist, async function(err,skres){
-                console.log("skres ===>",skres.success);
-                if(skres.success==true){
-                    /////////Update Stock//////////
-                    let resobj = {
-                        success: true,
-                        status: true,
-                        message: "stock updated successfully"
-                    };
-                    result(null, resobj);
-                }else{
-                    let resobj = {
-                        success: true,
-                        status: true,
-                        message: "something went wrong plz try again"
-                    };
-                    result(null, resobj);
-                }
-            });
+            if(getother.length>0){
+                var sklist = [];
+                sklist.push({"stockid":req.stockid,"vpid":req.vpid,"product_name":getother[0].Productname,"cat_id":getother[0].catid,"category_name":getother[0].catagory_name,"scl1_id":getother[0].scl1_id,"subcategoryl1_name":getother[0].subcatL1name,"scl2_id":getother[0].scl2_id,"subcategoryl2_name":getother[0].subcatL2name,"price":getother[0].mrp,"missing_quantity":missing_quantity,"boh":getother[0].boh,"actual_quantity":actual_quantity,"in_sorting":getother[0].insorting,"type":type,"wastage":wastage,"wastage_image":wastage_image,"zone_id":req.zone_id,"commend":getother[0].Productname,"weight":getother[0].weight});
+                /////////create stockkeeping//////////
+                Stockkeeping.createStockKeeping(sklist, async function(err,skres){
+                    if(skres.success==true){
+                        /////////Update Stock//////////
+                        var stockupdatequery = "update Stock set quantity="+actual_quantity+" where stockid="+req.stockid;
+                        var stockupdate = await query(stockupdatequery);
+                        if(stockupdate.affectedRows>0){
+                            let resobj = {
+                                success: true,
+                                status: true,
+                                message: "stock updated successfully"
+                            };
+                            result(null, resobj);
+                        }else{
+                            let resobj = {
+                                success: true,
+                                status: true,
+                                message: "something went wrong plz try again in stock"
+                            };
+                            result(null, resobj);
+                        }                    
+                    }else{
+                        let resobj = {
+                            success: true,
+                            status: true,
+                            message: "something went wrong plz try again in stock keeping"
+                        };
+                        result(null, resobj);
+                    }
+                });
+            }else{
+                let resobj = {
+                    success: true,
+                    status: false,
+                    message: "no stock found"
+                };
+                result(null, resobj);
+            }
         }else{
             let resobj = {
                 success: true,
                 status: false,
-                message: "no stock found"
+                message: "already in stockkeeping"
             };
             result(null, resobj);
         }
@@ -179,24 +199,52 @@ StockKeeping.stockkeeping_view =async function stockkeeping_view(req,result) {
 /////////Stockkeeping Edit///////////
 StockKeeping.stockkeeping_edit =async function stockkeeping_edit(req,result) {
     if(req.zone_id && req.skid){
-        var updateskquery = "update StockKeeping set actual_quantity="+req.actual_quantity+",missing_quantity="+req.missing_quantity+",wastage="+req.wastage+",wastage_image="+req.wastage_image+",type="+req.type+" where zoneid="+req.zone_id+" and skid="+req.skid;
-        var updatesk = await query(updateskquery);
-        if(updatesk.affectedRows>0){          
-            /////////Update Stock//////////
-            let resobj = {
-                success: true,
-                status: true,
-                mesage: "updated successfully"
-            };
-            result(null, resobj);               
+        var checkskquery = "select * from StockKeeping where zoneid="+req.zone_id+" and skid="+req.skid;
+        var checksk = await query(checkskquery);
+        if(checksk.length>0){
+            var updateskquery = "update StockKeeping set actual_quantity="+req.actual_quantity+",missing_quantity="+req.missing_quantity+",wastage="+req.wastage+",wastage_image="+req.wastage_image+",type="+req.type+" where zoneid="+req.zone_id+" and skid="+req.skid;
+            var updatesk = await query(updateskquery);
+            if(updatesk.affectedRows>0){          
+                /////////Update Stock//////////
+                var stockupdatequery = "update Stock set quantity="+req.actual_quantity+" where stockid="+checksk[0].stockid;
+                var stockupdate = await query(stockupdatequery);
+                if(stockupdate.affectedRows>0){
+                    let resobj = {
+                        success: true,
+                        status: true,
+                        mesage: "updated successfully"
+                    };
+                    result(null, resobj);
+                }else{
+                    let resobj = {
+                        success: true,
+                        status: false,
+                        message: "something went wrong plz try again in Stock"
+                    };
+                    result(null, resobj);
+                }
+                let resobj = {
+                    success: true,
+                    status: true,
+                    mesage: "updated successfully"
+                };
+                result(null, resobj);               
+            }else{
+                let resobj = {
+                    success: true,
+                    status: false,
+                    message: "something went wrong plz try again"
+                };
+                result(null, resobj);
+            }
         }else{
             let resobj = {
                 success: true,
-                status: false,
-                message: "something went wrong plz try again"
+                status: true,
+                mesage: "invalid skid"
             };
             result(null, resobj);
-        }
+        }        
     }else{
         let resobj = {
             success: true,
@@ -210,24 +258,55 @@ StockKeeping.stockkeeping_edit =async function stockkeeping_edit(req,result) {
 /////////Stockkeeping Delete///////////
 StockKeeping.stockkeeping_delete =async function stockkeeping_delete(req,result) {
     if(req.zone_id && req.skid){
-        var updateskquery = "update StockKeeping set delete_status=1 where zoneid="+req.zone_id+" and skid="+req.skid;
-        var updatesk = await query(updateskquery);
-        if(updatesk.affectedRows>0){          
-            /////////Update Stock//////////
-            let resobj = {
-                success: true,
-                status: true,
-                mesage: "updated successfully"
-            };
-            result(null, resobj);               
+        var checkskquery = "select * from StockKeeping where zoneid="+req.zone_id+" and skid="+req.skid;
+        var checksk = await query(checkskquery);
+        if(checksk.length>0){ 
+            if(checksk[0].delete_status==0){
+                var updateskquery = "update StockKeeping set delete_status=1 where zoneid="+req.zone_id+" and skid="+req.skid;
+                var updatesk = await query(updateskquery);
+                if(updatesk.affectedRows>0){          
+                    /////////Update Stock//////////
+                    var stockupdatequery = "update Stock set quantity="+checksk[0].boh+" where stockid="+checksk[0].stockid;
+                    var stockupdate = await query(stockupdatequery);
+                    if(stockupdate.affectedRows>0){
+                        let resobj = {
+                            success: true,
+                            status: true,
+                            mesage: "updated successfully"
+                        };
+                        result(null, resobj);
+                    }else{
+                        let resobj = {
+                            success: true,
+                            status: false,
+                            message: "something went wrong plz try again in Stock"
+                        };
+                        result(null, resobj);
+                    }                                   
+                }else{
+                    let resobj = {
+                        success: true,
+                        status: false,
+                        message: "something went wrong plz try again in StockKeeping"
+                    };
+                    result(null, resobj);
+                }
+            }else{
+                let resobj = {
+                    success: true,
+                    status: true,
+                    mesage: "already in delete state"
+                };
+                result(null, resobj);
+            }
         }else{
             let resobj = {
                 success: true,
-                status: false,
-                message: "something went wrong plz try again"
+                status: true,
+                mesage: "invalid skid"
             };
             result(null, resobj);
-        }
+        }        
     }else{
         let resobj = {
             success: true,
