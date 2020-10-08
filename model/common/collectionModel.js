7bf08cf64a44d91ad6781d85fbe4625162469646
kirubarajm <kirubarajmathavan@gmail.com>
2020-10-08 19:47:33 +0530

    collection show in app page completed

--- model/common/collectionModel.js
+++ model/common/collectionModel.js
@@ -54,7 +54,7 @@ if (get_nearby_zone.length !=0) {
             res[i].clickable=false;
           }
 
-          console.log("res[i].classification_type",res[i].classification_type);
+          // console.log("res[i].classification_type",res[i].classification_type);
           if (res[i].classification_type==1) { 
             //brand
            product_query = "select pm.*,pl.*  from  ProductMaster pm left join Product_live pl on pl.pid=pm.pid left join Brand br on br.id=pm.brand  where br.id = '"+res[i].classification_id+"' and pl.live_status=1"
